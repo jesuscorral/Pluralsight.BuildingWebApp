6556d1971b1459c4dc40ecd2492dab9e83df7942
Jesus Corral <jesus.corral@innovation.group>
2017-11-26 21:25:19 +0100

    MVC 6

---
+++ TheWorld/Controllers/Web/AppController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using TheWorld.Services;
+using TheWorld.ViewModels;
+
+namespace TheWorld.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        private IMailService MailService;
+        private IConfigurationRoot _config;
+
+        public AppController(IMailService mailService, IConfigurationRoot config)
+        {
+            MailService = mailService;
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (model.Email.Contains("aol.com"))
+            {
+                ModelState.AddModelError("Email", "We don't support AOL addresses");
+            }
+            if (ModelState.IsValid)
+            {
+                MailService.SendMail(_config["MailSettings:ToAddress"], model.Email, "From TheWorld", model.Message);
+
+                ModelState.Clear();
+                ViewBag.UserMessage = "Message Sent!";
+            }
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ TheWorld/Services/MailService.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace TheWorld.Services
+{
+    public class MailService : IMailService
+    {
+        public void SendMail(string to, string from, string subject, string body)
+        {
+
+            Debug.WriteLine($"Sending mail to: {to}, from {from} Subject: {subject}, body: {body}");
+        }
+    }
+}
